0ac2ac87690fafde5896a4ddd9dfe033b2851d45
Andrew Spada <spada.andrew.j@gmail.com>
2024-03-04 17:34:24 -0500

    whitespace fixes

--- configuration.nix
+++ configuration.nix
@@ -12,16 +12,15 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.gc = {
-  	 automatic = true;
-	 dates = "Mon *-*-* 03:15:00";
+    automatic = true;
+    dates = "Mon *-*-* 03:15:00";
   };
-  
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "PDP-11";
- 
+
   time.timeZone = "America/New_York";
 
   hardware.opengl = {
@@ -69,7 +68,7 @@
   };
 
   hardware.onlykey.enable = true;
-  
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
@@ -87,6 +86,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
-
