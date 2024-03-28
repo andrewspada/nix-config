7bf2f758f0c6aca8074c4e83073b58b24eec1c7d
Andrew Spada <spada.andrew.j@gmail.com>
2024-03-28 14:41:55 -0400

    macbook: add work around for emacs PATH

--- home-config/andrew-macbook.nix
+++ home-config/andrew-macbook.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ config, ... }:
 
 {
   imports = [ ./my-config.nix ];
   my-config.installFirefox = false;
+
+  # workaround, because GUI applications do not seem to have the right
+  # PATH set when launched from the launcher.
+  programs.emacs.extraConfig = ''
+    (setenv "PATH" "${config.home.homeDirectory}/.ghcup/bin:${config.home.homeDirectory}/.cabal/bin:${config.home.homeDirectory}/.nix-profile/bin:/etc/profiles/per-user/${config.home.username}/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin")
+  '';
 }
