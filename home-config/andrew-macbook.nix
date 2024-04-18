5fdc762d77a1521bdda69399b367856b2114e0fe
Andrew Spada <spada.andrew.j@gmail.com>
2024-04-18 14:37:28 -0400

    macbook: enable exec-path-from-shell in emacs

--- home-config/andrew-macbook.nix
+++ home-config/andrew-macbook.nix
@@ -7,6 +7,6 @@
   # workaround, because GUI applications do not seem to have the right
   # PATH set when launched from the launcher.
   programs.emacs.extraConfig = ''
-    (setenv "PATH" "${config.home.homeDirectory}/.ghcup/bin:${config.home.homeDirectory}/.cabal/bin:${config.home.homeDirectory}/.nix-profile/bin:/etc/profiles/per-user/${config.home.username}/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin")
+    (exec-path-from-shell-initialize)
   '';
 }
